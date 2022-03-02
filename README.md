543693b1b12fa23eea649cc64970b1e75af3305e
Luiz Henrique <luizhf42@gmail.com>
2022-03-02 16:28:20 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# quiz-app
+ A quiz app using Vue.js and the OpenTDB API.
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Oi</h1>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  font-size: 3rem;
+}
+</style>
